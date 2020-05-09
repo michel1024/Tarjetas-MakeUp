c4b444b258c2f7b5653e1f4a15dd8d75c6972c3b
Andres Acosta Hernandez <aacosta@sistemas-expertos.com>
2020-05-08 23:47:55 -0500

    version1

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.tarjeta1').animate({"margin-top": 0,
+    "opacity": 1}, 800);
+
+    $('.tarjeta2').animate({
+        "margin-bottom": 0, 
+        "opacity": 1}, 800);
+
+
+
+    $(".img-principal").animate({"width": 250+"px"}, 1000);
+});
